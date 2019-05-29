7f903d370cf347f8cb2cca44b0b5ee890a74e00c
Fang Zijian <eimlfang@126.com>
2019-05-29 14:23:38 +0800

    split configs

--- init.el
+++ init.el
@@ -1,48 +1,9 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
-;; '("melpa" . "https://melpa.org/packages/")
-;; http://elpa.emacs-china.org/melpa/
+(require 'init-packages)
 
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (package-initialize)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-  )
-
-(require 'cl)
-
-;; add whatever packages you want here
-(defvar eimlfang/packages '(
-			    company
-			    monokai-theme
-			    hungry-delete
-			    php-mode
-			    swiper
-			    counsel
-			    smartparens
-			    js2-mode
-			    rust-mode
-	nodejs-repl
-        exec-path-from-shell
-			    ) "Default packages")
-
-(setq package-selected-packages eimlfang/packages)
-
-(defun eimlfang/packages-installed-p()
-  (loop for pkg in eimlfang/packages
-	when (not (package-installed-p pkg)) do (return nil)
-	finally (return t)))
-
-  (unless (eimlfang/packages-installed-p)
-    (message "%s" "Refreshing package datbase...")
-    (package-refresh-contents)
-    (dolist (pkg eimlfang/packages)
-      (when (not (package-installed-p pkg))
-	(package-install pkg))))
-
+;; 关闭提示音
+(setq ring-bell-function 'ignore)
 ;; 关闭工具栏
 (tool-bar-mode -1)
 ;; 关闭滚动条
@@ -60,25 +21,21 @@
 ;; 绑定F2到快速打开init.el
 (global-set-key (kbd "<f2>") 'open-my-init-file)
 
-;; 最近打开文件
-(require 'recentf)
-(recentf-mode 1)
+
+;; org文档添加语法高亮
+(require 'org)
+(setq org-src-fontify-natively t)
+
 (setq recentf-max-menu-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
-
-
-;; 全局打开company-mode
-(global-company-mode t)
 
 ;; 设置光标样式
 (setq-default cursor-type 'bar)
 
 ;; 禁止备份文件
 (setq make-backup-files nil)
+(setq auto-save-default nil)
 
-;; org文档添加语法高亮
-(require 'org)
-(setq org-src-fontify-natively t)
 
 (delete-selection-mode t)
 
@@ -91,45 +48,10 @@
 ;; 显示当前行
 (global-hl-line-mode t)
 
-;; 加载nomokai主题
-(load-theme 'monokai t)
+;; 从磁盘重新加载文件
+(global-auto-revert-mode t)
 
-;; hungry-delete
-(require 'hungry-delete)
-(global-hungry-delete-mode)
 
-;; 括号匹配
-(require 'smartparens-config)
-;; (add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-(smartparens-global-mode t)
-
-;; php-mode
-(require 'php-mode)
-
-;; rust-mode
-(require 'package)
-(setq auto-mode-alist
-      (append
-       '(("\\.rs\\'" . rust-mode))
-       auto-mode-alist))
-
-;; config for js files
-(setq auto-mode-alist
-      (append
-       '(("\\.js\\'" . js2-mode))
-       auto-mode-alist))
-
-;; nodejs-repl
-(require 'nodejs-repl)
-
-;; exec-path-from-shell
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
-    
-    
-;; swiper
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
 ;; enable this if you want `swiper' to use it
 ;; (setq search-default-mode #'char-fold-to-regexp)
---
+++ lisp/init-packages.el
@@ -0,0 +1,86 @@
+;; '("melpa" . "https://melpa.org/packages/")
+;; http://elpa.emacs-china.org/melpa/ ;; 国内镜像源
+(require 'cl)
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  )
+
+
+;; add whatever packages you want here
+(defvar eimlfang/packages '(
+			    company
+			    monokai-theme
+			    hungry-delete
+			    php-mode
+			    swiper
+			    counsel
+			    smartparens
+			    js2-mode
+			    rust-mode
+			    nodejs-repl
+			    exec-path-from-shell
+			    popwin
+			    ) "Default packages")
+
+(setq package-selected-packages eimlfang/packages)
+
+(defun eimlfang/packages-installed-p()
+  (loop for pkg in eimlfang/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+  (unless (eimlfang/packages-installed-p)
+    (message "%s" "Refreshing package datbase...")
+    (package-refresh-contents)
+    (dolist (pkg eimlfang/packages)
+      (when (not (package-installed-p pkg))
+	(package-install pkg))))
+
+;; exec-path-from-shell
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; nodejs-repl
+(require 'nodejs-repl)
+;; php-mode
+(require 'php-mode)
+;; rust-mode
+(require 'package)
+(setq auto-mode-alist
+      (append
+       '(("\\.rs\\'" . rust-mode))
+       auto-mode-alist))
+
+;; 配置js2-mode
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+;; hungry-delete
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+;; 括号匹配
+(require 'smartparens-config)
+;; (add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(smartparens-global-mode t)
+;; 最近打开文件
+(require 'recentf)
+(recentf-mode 1)
+
+(require 'popwin)
+(popwin-mode t)
+
+;; swiper
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+;; 全局打开company-mode
+(global-company-mode t)
+
+;; 加载nomokai主题
+(load-theme 'monokai t)
+
+(provide 'init-packages)
